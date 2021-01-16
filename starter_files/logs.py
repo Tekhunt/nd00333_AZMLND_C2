124e05d66d758a695e7d3b2b039dd9ee91b29784
Chiazam Ochiegbu <chizzydmec@gmail.com>
2021-01-17 00:32:16 +0100

    Saved the banking dataset

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -7,8 +7,14 @@ ws = Workspace.from_config()
 # Set with the deployment name
 name = ""
 
+# Enable application insight
+
+
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+
+service.update(enable_app_insights=True)
+
 logs = service.get_logs()
 
 for line in logs.split('\n'):
